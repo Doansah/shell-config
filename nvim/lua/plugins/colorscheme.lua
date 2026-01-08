54ccab1caa58154a0e94b2aa42c057c0b45a2f8d
Dillon Ansah <dillondoa@gmail.com>
2026-01-08 01:50:43 +0000

    Ashen ColorScheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
+  { "ficcdaf/ashen.nvim" },
   {
-    "xero/miasma.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme miasma")
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ashen",
+    },
   },
 }
